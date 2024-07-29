adaf75928d0e77fc776a0d7ed0a4ddce12a3ccf8
NormanJaimes <normane_@hotmail.com>
2024-07-29 14:28:51 -0500

    Agregando useContext y boilerPLate de código useContext

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,9 @@
 import { useMemo, useState } from 'react';
+import { useBudget } from '../hooks/useBudget';
 
 export default function BudgetForm() {
   const [budget, setBudget] = useState(0);
+  const { dispatch } = useBudget();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBudget(e.target.valueAsNumber);
@@ -9,8 +11,15 @@ export default function BudgetForm() {
   const isValid = useMemo(() => {
     return isNaN(budget) || budget <= 0;
   }, [budget]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log('Agreegandi presupuesto');
+    dispatch({ type: 'add-budget', payload: { budget } });
+  };
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <div className="flex flex-col space-y-5">
         <label
           htmlFor="budget"
