0226300b18749c6981b38a9a4b79fd55b6723e4f
NormanJaimes <normane_@hotmail.com>
2024-07-30 19:39:19 -0500

    Definiendo la acción para nuevos gastos

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -5,6 +5,7 @@ import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
 import { DarfExpense } from '../types';
 import { Value } from '../types/index';
+import ErrorMessage from './ErrorMessage';
 
 export default function ExpenseForm() {
   const [expense, setExpense] = useState<DarfExpense>({
@@ -13,6 +14,7 @@ export default function ExpenseForm() {
     category: '',
     date: new Date(),
   });
+  const [error, setError] = useState('');
 
   const hangleChange = (
     e:
@@ -32,11 +34,23 @@ export default function ExpenseForm() {
       date: value,
     });
   };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // validar
+    if (Object.values(expense).includes('')) {
+      setError('Todos los campos son obligatorios');
+      return;
+    }
+    console.log('Bien...');
+  };
   return (
-    <form action="" className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
         Nuevo Gasto
       </legend>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <div className="flex flex-col gap-2">
         <label htmlFor="expenseName" className="text-xl">
