ff00e1bf572beddc90827ef2e61c141996c85f1a
NormanJaimes <normane_@hotmail.com>
2024-07-31 12:15:47 -0500

    Reiniciar App

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -8,7 +8,8 @@ export type BudgetActions =
   | { type: 'add-expense'; payload: { expense: DraftExpense } }
   | { type: 'remove-expense'; payload: { id: Expense['id'] } }
   | { type: 'get-expense-by-id'; payload: { id: Expense['id'] } }
-  | { type: 'update-expense'; payload: { expense: Expense } };
+  | { type: 'update-expense'; payload: { expense: Expense } }
+  | { type: 'reset-app' };
 
 export type BudgetState = {
   budget: number;
@@ -95,6 +96,14 @@ export const budgetReducer = (
           : expense
       ),
       modal: false,
+      editingid: '',
+    };
+  }
+  if (action.type === 'reset-app') {
+    return {
+      budget: 0,
+      modal: false,
+      expenses: [],
       editingid: '',
     };
   }
