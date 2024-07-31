2172f90053f1f18bc5e0615872e59402640c2612
NormanJaimes <normane_@hotmail.com>
2024-07-30 19:12:17 -0500

    Actualizando el estado dependiendo el valor del form en nuevo gasto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -2,11 +2,36 @@ import { categories } from '../data/categories';
 import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
+import { useState } from 'react';
+import { DarfExpense } from '../types';
+import { Value } from '../types/index';
 
 export default function ExpenseForm() {
-  type ValuePiece = Date | null;
+  const [expense, setExpense] = useState<DarfExpense>({
+    amount: 0,
+    expenseName: '',
+    category: '',
+    date: new Date(),
+  });
 
-  type Value = ValuePiece | [ValuePiece, ValuePiece];
+  const hangleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    const isAmountField = ['amount'].includes(name);
+    setExpense({
+      ...expense,
+      [name]: isAmountField ? +value : value,
+    });
+  };
+  const handleChangeDate = (value: Value) => {
+    setExpense({
+      ...expense,
+      date: value,
+    });
+  };
   return (
     <form action="" className="space-y-5">
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
@@ -23,6 +48,8 @@ export default function ExpenseForm() {
           placeholder="Agerga el nombre del gasto"
           className="bg-slate-100 p-2"
           name="expenseName"
+          value={expense.expenseName}
+          onChange={hangleChange}
         />
       </div>
       <div className="flex flex-col gap-2">
@@ -35,13 +62,21 @@ export default function ExpenseForm() {
           placeholder="Agerga la cantidad del gasto: ej. 100"
           className="bg-slate-100 p-2"
           name="amount"
+          value={expense.amount}
+          onChange={hangleChange}
         />
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="amount" className="text-xl">
           Categoríá:
         </label>
-        <select id="category" className="bg-slate-100 p-2" name="category">
+        <select
+          id="category"
+          className="bg-slate-100 p-2"
+          name="category"
+          value={expense.category}
+          onChange={hangleChange}
+        >
           <option value="">-- Seleccione --</option>
           {categories.map((category) => (
             <option value={category.id} key={category.id}>
@@ -54,7 +89,11 @@ export default function ExpenseForm() {
         <label htmlFor="amount" className="text-xl">
           Fecha Gasto:
         </label>
-        <DatePicker className="bg-slate-100 p-2 border-0" />
+        <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          value={expense.date}
+          onChange={handleChangeDate}
+        />
       </div>
 
       <input
